f9f5b6bfeca003bb60341619abb4ef61a7121e0d
chenglei <chenglei01@netease.corp.com>
2022-10-11 11:33:49 +0800

    lib里面添加flover

--- app/src/main/java/com/chenglei/vest/MainActivity.kt
+++ app/src/main/java/com/chenglei/vest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.chenglei.vest
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.chenglei.vestlib.VestLibToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,8 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(
             R.id.btn1
         ).setOnClickListener {
-            VestLog().log(this@MainActivity)
+//            VestLog().log(this@MainActivity)
+            VestLibToast().toast(this@MainActivity)
         }
     }
 }
---
+++ biz_toast/src/vesta/java/com/chenglei/vestlib/VestLibToast.kt
@@ -0,0 +1,10 @@
+package com.chenglei.vestlib
+
+import android.content.Context
+import android.widget.Toast
+
+class VestLibToast {
+    fun toast(context: Context) {
+        Toast.makeText(context, "vest lib a", Toast.LENGTH_SHORT).show()
+    }
+}
